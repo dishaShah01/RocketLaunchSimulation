b84eaecb0ad90bca7fb253d104fb477d1b916525
Hinal Pujara <hinal.pujara@somaiya.edu>
2021-11-26 12:31:01 +0530

    Added Comments

--- Rocket_launch.py
+++ Rocket_launch.py
@@ -5,15 +5,19 @@ from OpenGL.GLU import *
 import math
 import random
 
-DEG2RAD = 3.14159/180
-count=0
-count1=0
-launch=0
-sky_color=0
-tx=0
-ty=0
-fumes=0
 
+# Variables declared
+DEG2RAD = 3.14159/180    #to convert angle from degree to rad
+count=0            #render all parts sequintially.
+count1=0
+launch=0        #state of the rocket launch
+sky_color=0        #chage the background
+tx=0            #translate along X-Axis
+ty=0            #translate along Y-Axis
+fumes=0         #state of the fumes of the rocket
+
+
+# Function to Display stars
 def stars():
     glColor3f(1.0,1.0,1.0)
     glPointSize(2)
@@ -25,6 +29,19 @@ def stars():
     glEnd()
 
 
+# Function to display moon
+def moon(radius):  
+    global tx,ty,DEG2RAD
+    glBegin(GL_POLYGON)
+    for i in range(359):
+        degInRad = i*DEG2RAD
+        glVertex2f(300+ty+math.cos(degInRad)*radius,500-tx+(math.sin(degInRad))*radius)
+    glEnd()
+    tx=tx+0.1
+    ty=ty+0.1
+
+
+# Function to display grass
 def grass():
     glColor3f(0.0,0.9,0.0)
     glPointSize(3)
@@ -35,6 +52,8 @@ def grass():
         glVertex2i(x,y)
     glEnd()
 
+
+# Function to control the rendering parts of rocket launch
 def control():
     global launch,sky_color,count,count1,tx,ty,fumes,DEG2RAD
     count1+=1
@@ -45,6 +64,8 @@ def control():
     elif (launch == 1 and count1 >= 1000000):
         Moving_Rocket()    
 
+
+# Function to display rocket scene on earth
 def Rocket_on_Ground():
     global launch,count1,DEG2RAD
     count1+=1
@@ -54,6 +75,7 @@ def Rocket_on_Ground():
         glClearColor(0.196078  ,0.6 ,0.8,1.0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
        
+        # grass
         glColor3f(0,0.3,0.0)
         glBegin(GL_POLYGON)
         glVertex2f(0.0,0.0)
@@ -64,6 +86,7 @@ def Rocket_on_Ground():
         glEnd()
         grass()
 
+        # launch pad
         glColor3f(0.0,0.0,0.0)
         glBegin(GL_POLYGON)
         glVertex2f(170,0.0)
@@ -80,6 +103,7 @@ def Rocket_on_Ground():
         glVertex2f(260,130.0)
         glEnd()
 
+        # Rocket body
         glColor3f(0.5,0.0 ,0.9)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,20.0)
@@ -88,12 +112,15 @@ def Rocket_on_Ground():
         glVertex2f(237.5,120.0)
         glEnd()
 
+        # Rocket top
         glColor3f(1.0,1.0,1.0)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,120.0)
         glVertex2f(262.5,120.0)
         glVertex2f(250,170.0)
         glEnd()
+
+        # boosters
         glColor3f(0.9,0.9,0.0)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,120.0)
@@ -115,6 +142,8 @@ def Rocket_on_Ground():
         glVertex2f(262.5,95.0)
         glVertex2f(282.5,95.0)
         glEnd()
+
+        # fumes
         glColor3f(1.0 ,0.5,0)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,20.0)
@@ -150,7 +179,9 @@ def Rocket_on_Ground():
         glVertex2f(262.5,15.0)
         glVertex2f(312.5,15.0)
         glEnd()
-        glBegin(GL_POLYGON) #Sky  
+
+        #Sky  
+        glBegin(GL_POLYGON) 
         glColor3f(0.4, 0.5, 1.0)
         glVertex2i(600, 600)
         glVertex2i(0, 600)
@@ -162,6 +193,8 @@ def Rocket_on_Ground():
         glutPostRedisplay()
         glFlush()
 
+
+# When rocket is in the sky but in earth's atmosphere
 def rocket_position(): 
 
     global count,fumes
@@ -169,7 +202,8 @@ def rocket_position():
     for i in range(200):
         glClearColor(0.196078,0.6 ,0.8,1.0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        glBegin(GL_POLYGON) #Sky  
+        #Sky 
+        glBegin(GL_POLYGON)  
         glColor3f(0.4, 0.5, 1.0)
         glVertex2i(600, 600)
         glVertex2i(0, 600)
@@ -177,19 +211,25 @@ def rocket_position():
         glVertex2i(0, 0)
         glVertex2i(600, 0)
         glEnd() 
+
+        # Rocket body
         glColor3f(0.5,0.0 ,0.9)
         glBegin(GL_POLYGON)
-        glVertex2f(237.5,20.0+i)
+        glVertex2f(237.5,20.0+i)        #Translates as the count increases.
         glVertex2f(262.5,20.0+i)
         glVertex2f(262.5,120.0+i)
         glVertex2f(237.5,120.0+i)
         glEnd()
+
+        # Rocket top
         glColor3f(1.0,1.0,1.0)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,120.0+i)
         glVertex2f(262.5,120.0+i)
         glVertex2f(250,170.0+i)
         glEnd()
+
+        # boosters
         glColor3f(0.9,0.9,0.0)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,120.0+i)
@@ -211,6 +251,8 @@ def rocket_position():
         glVertex2f(262.5,95.0+i)
         glVertex2f(282.5,95.0+i)
         glEnd()
+
+        # fumes
         glColor3f(0.556863 ,0.137255  ,0.419608)
         glBegin(GL_POLYGON)
         glVertex2f(237.5,20.0+i)
@@ -297,11 +339,14 @@ def rocket_position():
         glutPostRedisplay()
         glFlush()
 
+
+# When Roket is in lower earth orbit
 def Moving_Rocket():    
     global sky_color,count,tx,ty
     count+=1
     
     for i in range(195,201):
+        # sky
         if(count>=50):
             glClearColor(0.0 ,0.0 ,0.0,1.0)
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
@@ -316,6 +361,8 @@ def Moving_Rocket():
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         if(count>=100):
             moon(20.0)	 
+        
+        # Rocket body
         if(count<=300):
             glColor3f(0.5,0.0 ,0.9)
             glBegin(GL_POLYGON)
@@ -324,10 +371,12 @@ def Moving_Rocket():
             glVertex2f(262.5,120.0+i)
             glVertex2f(237.5,120.0+i)
             glEnd()
+
+        #core
         if(count>=150):
             k = i
             glColor3f(1.0,1.0,1.0)
-            glBegin(GL_POLYGON)#core
+            glBegin(GL_POLYGON)
             glVertex2f(237.5,150.0+k)
             glVertex2f(252.5,150.0+k)
             glVertex2f(252.5,120.0+k)
@@ -343,13 +392,17 @@ def Moving_Rocket():
             glVertex2f(227.5,130.0+k)
             glVertex2f(262.5,130.0+k)
             glEnd()
+        
+        #Rocket top
         else:
             glColor3f(1.0,1.0,1.0)
-            glBegin(GL_POLYGON)#front
+            glBegin(GL_POLYGON)
             glVertex2f(237.5,120.0+i)
             glVertex2f(262.5,120.0+i)
             glVertex2f(250,170.0+i)
             glEnd()	
+        
+        # boosters
         if(count<=120):
             glColor3f(0.9,0.9,0.0)
             glBegin(GL_POLYGON)
@@ -372,6 +425,8 @@ def Moving_Rocket():
             glVertex2f(262.5,95.0+i)
             glVertex2f(282.5,95.0+i)
             glEnd()
+
+        # fumes.
         if(count<=110):
             glColor3f(0.556863 ,0.137255  ,0.419608)
             glBegin(GL_POLYGON)
@@ -393,22 +448,13 @@ def Moving_Rocket():
     glutPostRedisplay()
     glFlush()
 
-def moon(radius):  
-    global tx,ty,DEG2RAD
-    glBegin(GL_POLYGON)
-    for i in range(359):
-        degInRad = i*DEG2RAD
-        glVertex2f(300+ty+math.cos(degInRad)*radius,500-tx+(math.sin(degInRad))*radius)
-    glEnd()
-    tx=tx+0.1
-    ty=ty+0.1
 
 def iterate():
     glClearColor(0.196078  ,0.6 ,0.8,1.0)
     glPointSize(1.0)
     gluOrtho2D(0.0,499.0,0.0,499.0)
 
-
+# to display rocket launch
 def Showscreen():
     global launch,sky_color,count,count1,tx,ty,fumes,DEG2RAD
     if (launch == 0):
